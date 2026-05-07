46cb689e6a0e6d553179075cdbe679ab6491adfc
Pawan Chauhan <pawan.ch67192@gmail.com>
2026-05-07 11:22:16 +0530

    Add files via upload

---
+++ question-2/src/pages/Contact.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div className="box">
+      <h2>Contact Page</h2>
+      <p>Welcome to the Contact Page</p>
+    </div>
+  );
+}
+
+export default Contact;
---
+++ question-2/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="box">
+      <h2>Home Page</h2>
+      <p>Welcome to the Home Page</p>
+    </div>
+  );
+}
+
+export default Home;
